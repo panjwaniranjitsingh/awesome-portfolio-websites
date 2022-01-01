e7aa97bc26ae8ba37edd61a8f9db6015ed40327b
Panjwani Ranjit Singh <panjwaniranjitsingh@gmail.com>
2022-01-01 19:57:11 +0530

    Update experience.js
    Added work experience

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -6,25 +6,25 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
+    title: "Game Programmer",
     cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    place: "Acid Trip Arts Studio Pvt. Ltd.",
+    time: "(March, 2021 - present)",
+    desp: "<li>Developed 10 hyper-casual games for clients using playcanvas and javascript.</li> <li>Interacted with clients to understand their needs for those particular hypercasual games.</li> <li>Co-ordinated with artist and colleagues to work as a team.</li> <li>Implemented games for mobile platform.</li>",
   },
   {
-    title: "Student Developer",
+    title: "Game Programmer Intern",
     cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    place: "Acid Trip Arts Studio Pvt. Ltd.",
+    time: "(December,2020 - February, 2021)",
+    desp: "<li>Studied Code of Existing Games.</li><li>Created prototype of 5 hypercasual games.</li><li>Successfully did modifications in existing games.</li>",
   },
   {
-    title: "Research Intern",
+    title: "IT Infrastructure & Operations",
     cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    place: "Goldwood Ply Industry Pvt Ltd",
+    time: "(2010 - 2020)",
+    desp:"<li>Worked on software implementation and maintenance.</li><li>Surpervised Operational Management.</li><li>Managed accounting and expenses.</li>",
   },
 ];
 
