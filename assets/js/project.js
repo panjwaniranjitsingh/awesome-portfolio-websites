74dc0bdc2aaed62377320d22049b653e16271db3
Panjwani Ranjit Singh <panjwaniranjitsingh@gmail.com>
2022-01-02 10:43:26 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -11,7 +11,6 @@ const projects = [
     description: "Classic Tic Tac Toe game with bot.",
     tagimg: "https://www.chrisgodber.co.uk/img/playcanvas.jpg",
     Previewlink: "https://games.acidtriparts.com/games_webbuild/ticTacToe1/",
-    Githublink: "",
   },
   {
     title: "Connect4",
@@ -19,7 +18,6 @@ const projects = [
     description: "An extended version of Tic-Tac-Toe with bot",
     tagimg: "https://www.chrisgodber.co.uk/img/playcanvas.jpg",
     Previewlink: "https://games.acidtriparts.com/games_webbuild/connect4WithBot1/",
-    Githublink: "",
   },
   {
     title: "Master Quiz",
@@ -27,7 +25,6 @@ const projects = [
     description: "A quiz game like Kaun Banega Crorepati",
     tagimg: "https://www.chrisgodber.co.uk/img/playcanvas.jpg",
     Previewlink: "https://games.acidtriparts.com/games_webbuild/masterQuiz1/",
-    Githublink: "",
   },
   {
     title: "Sniper Shooter 3D",
@@ -35,7 +32,7 @@ const projects = [
     description: "First Person Shooter game with zombies",
     tagimg: "https://www.chrisgodber.co.uk/img/playcanvas.jpg",
     Previewlink: "https://games.acidtriparts.com/games_webbuild/sniperShooter3d1/",
-    Githublink: "",
+    
   },
   {
     title: "Snakes & Ladders",
@@ -43,7 +40,7 @@ const projects = [
     description: "Typical cardboard game of snakes and ladders with bot",
     tagimg: "https://www.chrisgodber.co.uk/img/playcanvas.jpg",
     Previewlink: "https://games.acidtriparts.com/games_webbuild/snakeAndLadder1/",
-    Githublink: "",
+    
   },
   {
     title: "Find Hidden Objects",
@@ -51,7 +48,7 @@ const projects = [
     description: "Find objects which are hidden in plain sight",
     tagimg: "https://www.chrisgodber.co.uk/img/playcanvas.jpg",
     Previewlink: "https://games.acidtriparts.com/games_webbuild/hiddenObjectLevelBased1/",
-    Githublink: "",
+    
   },
   
 ];
@@ -69,11 +66,11 @@ const showCards = () => {
             <div class="data">
               <div class="content">
               <div class="title-div">
-                <h1 class="title"><a href="#">${title}</a></h1>
+                <h1 class="title"><a href="${Previewlink}">${title}</a></h1>
                 </div>
             <ul class="menu-content"><br>
                   <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
-                  <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
+        
                 </ul>
               </div>
             </div>
